db8734207c65244bc20a92224d3f183267719fa6
junzuw <167156934+junzuw@users.noreply.github.com>
2026-04-20 16:21:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'donasi-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  'https://cdn-icons-png.flaticon.com/512/3771/3771278.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetching assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
